79e9472a55c04fb0ae7108b0412896de7bd3f0d0
ShreyaSirgound <shreya.sirgound@student.tdsb.on.ca>
2023-01-04 17:00:58 -0500

    Create MainFrame.java

---
+++ src/MainFrame.java
@@ -0,0 +1,48 @@
+import java.awt.EventQueue;
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
+import java.io.IOException;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class MainFrame {
+    public MainFrame() {
+        JFrame frame = new JFrame("HomePage");
+        setMainFrame(frame);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(1280, 720);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+
+    /**
+     * The main GUI frame of the application
+     */
+    private static Frame mainFrame;
+
+    public static Frame getMainFrame() {
+		return mainFrame;
+	}
+
+	public static void setMainFrame(Frame newMainFrame) {
+		mainFrame = newMainFrame;
+	}
+
+    public static void main(String[] args) {
+
+		// Set the look and feel of the GUI.
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+			ex.printStackTrace();
+			System.err.println("Unsupported operating system.");
+		}
+
+        // Create and open the title GUI.
+		EventQueue.invokeLater(TitleFrame::new);
+    }
+}
